0c0be9fc4e6c789d48b7ffb57a0c88a4ef4d7667
mainak55512 <mbhattacharjee432@gmail.com>
2024-12-02 23:57:23 +0530

    branch handling

---
+++ tui/available_branches_tui.go
@@ -0,0 +1,82 @@
+package tui
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var (
+	SelectedBranch *string
+)
+
+type AvailableBranchModel struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func InitialAvailableBranchModel(branchList []string) AvailableBranchModel {
+	return AvailableBranchModel{
+		choices:  branchList,
+		selected: make(map[int]struct{}),
+	}
+}
+func (m AvailableBranchModel) Init() tea.Cmd {
+	return nil
+}
+func (m AvailableBranchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			if _, ok := m.selected[m.cursor]; ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+			SelectedBranch = &m.choices[m.cursor]
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+func (m AvailableBranchModel) View() string {
+	s := "\nWhat to do?\n\n"
+
+	for i, choice := range m.choices {
+
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "✓"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func StartAvailableBranchOptions(branchList []string) string {
+	p := tea.NewProgram(InitialAvailableBranchModel(branchList))
+	p.Run()
+	return *SelectedBranch
+}
---
+++ tui/branch_menu_tui.go
@@ -0,0 +1,86 @@
+package tui
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var (
+	SelectedBranchOption *string
+)
+
+type BranchModel struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func InitialBranchModel() BranchModel {
+	return BranchModel{
+		choices: []string{
+			"Switch Branch",
+			"Create & Switch Branch",
+			"Delete Branch",
+		},
+		selected: make(map[int]struct{}),
+	}
+}
+func (m BranchModel) Init() tea.Cmd {
+	return nil
+}
+func (m BranchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			if _, ok := m.selected[m.cursor]; ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+			SelectedBranchOption = &m.choices[m.cursor]
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+func (m BranchModel) View() string {
+	s := "\nWhat to do?\n\n"
+
+	for i, choice := range m.choices {
+
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "✓"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func StartBranchModel() string {
+	p := tea.NewProgram(InitialBranchModel())
+	p.Run()
+	return *SelectedBranchOption
+}
